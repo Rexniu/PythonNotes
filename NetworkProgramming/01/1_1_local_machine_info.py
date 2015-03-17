893bd8c5af2313493fb77f4c33e2f8f8950caada
Rexniu <caicainishishui@gmail.com>
2015-03-17 23:10:33 +0800

    01

---
+++ NetworkProgramming/01/1_1_local_machine_info.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import socket
+
+def print_machine_info():
+	host_name = socket.gethostname()
+	ip_address = socket.gethostbyname(host_name)
+	print "Host name: %s" % host_name
+	print "IP address: %s" % ip_address
+
+if __name__ == '__main__':
+	print_machine_info()
+	
