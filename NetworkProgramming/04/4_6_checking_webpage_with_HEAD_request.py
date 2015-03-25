ef7d3d7a20b61f0591a160b01bcb365cd4dfbfba
Rexniu <caicainishishui@gmail.com>
2015-03-25 18:44:17 +0800

    checking_webpage_with_HEAD_request

---
+++ NetworkProgramming/04/4_6_checking_webpage_with_HEAD_request.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import argparse
+import httplib
+import urlparse
+import re
+import urllib
+
+DEFAULT_URL = 'http://www.python.org'
+HTTP_GOOD_CODES = [httplib.OK,httplib.FOUND,httplib.MOVED_PERMANENTLY]
+
+def get_server_status_code(url):
+	"""
+	Download just the header of a URL and
+	return the server's status code.
+	"""
+	host,path = urlparse.urlparse(url)[1:3]
+	try:
+		conn = httplib.HTTPConnection(host)
+		conn.request('HEAD',path)
+		return conn.getresponse().status
+	except StandardError:
+		return None
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Example HEAD Request')
+	parser.add_argument('--url',action="store",dest="url",default=DEFAULT_URL)
+	given_args = parser.parse_args()
+	url = given_args.url
+	if get_server_status_code(url) in HTTP_GOOD_CODES:
+		print "Server:%s status is OK: " %url
+	else:
+		print "Server:%s status is NOT OK!" %url
