984d1ddf57414f559d4a3170aad0415ba6657dd8
Rexniu <caicainishishui@gmail.com>
2015-04-08 12:28:07 +0800

    excute_remote_telnet

---
+++ NetworkProgramming/07/7_1_execute_remote_telnet_cmd.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env/ python
+
+import getpass
+import sys
+import telnetlib
+
+HOST = "localhost"
+
+def run_telnet_session():
+	usr = raw_input("Enter your remote account")
+	password = getpass.getpass()
+	
+	session = telnetlib.Telnet(HOST)
+	
+	session.read_until("login:")
+	session.write(user + "\n")
+	if password:
+		session.read_until("Password:")
+		session.write(password+"\n")
+	
+	session.write("ls\n")
+	session.write("exit\n")
+
+	print session.read_all()
+
+if __name__ == '__main__':
+	run_telnet_session()
